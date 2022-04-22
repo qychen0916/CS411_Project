ad4a556ebeb7cc1b54d486bbab06841288be325a
katiehansen <kmhansen@bu.edu>
2022-04-22 11:07:03 -0400

    Working YouTube API search code samples

--- prototype/youtube_search_sample.py
+++ prototype/youtube_search_sample.py
@@ -1,41 +1,49 @@
-# -*- coding: utf-8 -*-
+from youtubesearchpython import VideosSearch
 
-# Sample Python code for youtube.search.list
-# See instructions for running these code samples locally:
-# https://developers.google.com/explorer-help/code-samples#python
+videosSearch = VideosSearch('Despacito', limit = 1)
 
-import os
+print(videosSearch.result())
 
-import google_auth_oauthlib.flow
-import googleapiclient.discovery
-import googleapiclient.errors
+# # -*- coding: utf-8 -*-
 
-scopes = ["https://www.googleapis.com/auth/youtube.force-ssl"]
+# # Sample Python code for youtube.search.list
+# # See instructions for running these code samples locally:
+# # https://developers.google.com/explorer-help/code-samples#python
 
-def main():
-    # Disable OAuthlib's HTTPS verification when running locally.
-    # *DO NOT* leave this option enabled in production.
-    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+# import os
 
-    api_service_name = "youtube"
-    api_version = "v3"
-    client_secrets_file = "api_keys.py"
+# import google_auth_oauthlib.flow
+# import googleapiclient.discovery
+# import googleapiclient.errors
+# from google.oauth2.credentials import Credentials
 
-    # Get credentials and create an API client
-    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
-        client_secrets_file, scopes)
-    credentials = flow.run_console()
-    youtube = googleapiclient.discovery.build(
-        api_service_name, api_version, credentials=credentials)
+# scopes = ["https://www.googleapis.com/auth/youtube.force-ssl"]
 
-    request = youtube.search().list(
-        part="snippet",
-        maxResults=25,
-        q="surfing"
-    )
-    response = request.execute()
+# def main():
+#     # Disable OAuthlib's HTTPS verification when running locally.
+#     # *DO NOT* leave this option enabled in production.
+#     os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
-    print(response)
+#     api_service_name = "youtube"
+#     api_version = "v3"
+#     client_secrets_file = "youtube_client_secrets.json"
 
-if __name__ == "__main__":
-    main()
+#     # Get credentials and create an API client
+#     # flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+#     #     client_secrets_file, scopes)
+
+#     credentials = Credentials.from_authorized_user_file(client_secrets_file, scopes)
+#     youtube = googleapiclient.discovery.build(
+#         api_service_name, api_version, credentials=credentials)
+
+#     request = youtube.search().list(
+#         part="snippet",
+#         maxResults=25,
+#         q="surfing"
+#     )
+#     response = request.execute()
+
+#     print(response)
+
+# if __name__ == "__main__":
+#     main()
